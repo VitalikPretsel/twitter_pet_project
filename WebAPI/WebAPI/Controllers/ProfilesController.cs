f76f297e5c71855e92b49c57aa479c566c4668c7
Vitalik Pretsel <v.pretsel@gmail.com>
2021-07-19 12:56:25 +0300

    profiles controller added

---
+++ WebAPI/WebAPI/Controllers/ProfilesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL.DataContext;
+using DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationContext appContext;
+        
+        public UsersController(ApplicationContext context)
+        {
+            appContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await appContext.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            User user = await appContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(user);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            appContext.Users.Add(user);
+            await appContext.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<User>> Put(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            if (!appContext.Users.Any(u => u.Id == user.Id))
+            {
+                return NotFound();
+            }
+            appContext.Update(user);
+            await appContext.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Post>> Delete(int id)
+        {
+            User user = await appContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            appContext.Remove(user);
+            await appContext.SaveChangesAsync();
+            return Ok(user);
+        }
+    }
+}
