cabf4312ecebd25f4ba8cbd8c3e3293da16ce0c0
Vitalik Pretsel <v.pretsel@gmail.com>
2021-07-19 13:02:45 +0300

    followings controller added

---
+++ WebAPI/WebAPI/Controllers/FollowingsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL.DataContext;
+using DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FollowingsController : ControllerBase
+    {
+        private readonly ApplicationContext appContext;
+        
+        public FollowingsController(ApplicationContext context)
+        {
+            appContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Following>>> Get()
+        {
+            return await appContext.Followings.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Following>> Get(int id)
+        {
+            Following following = await appContext.Followings.FirstOrDefaultAsync(f => f.Id == id);
+            if (following == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(following);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<Following>> Following(Following following)
+        {
+            if (following == null)
+            {
+                return BadRequest();
+            }
+            appContext.Followings.Add(following);
+            await appContext.SaveChangesAsync();
+            return Ok(following);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Following>> Put(Following following)
+        {
+            if (following == null)
+            {
+                return BadRequest();
+            }
+            if (!appContext.Followings.Any(f => f.Id == following.Id))
+            {
+                return NotFound();
+            }
+            appContext.Update(following);
+            await appContext.SaveChangesAsync();
+            return Ok(following);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Post>> Delete(int id)
+        {
+            Post post = await appContext.Posts.FirstOrDefaultAsync(p => p.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            appContext.Remove(post);
+            await appContext.SaveChangesAsync();
+            return Ok(post);
+        }
+    }
+}
