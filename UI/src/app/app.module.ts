6632577259b5363a39a6da24896ef03585ce1394
Vitalik Pretsel <v.pretsel@gmail.com>
2021-08-12 16:59:39 +0300

    added ngx-infinite-scroll and infinite-scroll component

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
+import { InfiniteScrollComponent } from './infinite-scroll/infinite-scroll.component';
 
 import { HttpRequestInterceptor} from './_helpers/http-request.interceptor';
 import { MaterialModule } from './material.module';
@@ -19,7 +21,8 @@ import { MaterialModule } from './material.module';
     AppComponent,
     LoginComponent,
     HomeComponent,
-    UsersComponent
+    UsersComponent,
+    InfiniteScrollComponent
   ],
   imports: [
     MaterialModule,
@@ -27,7 +30,8 @@ import { MaterialModule } from './material.module';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    InfiniteScrollModule
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true }],
   bootstrap: [AppComponent]
