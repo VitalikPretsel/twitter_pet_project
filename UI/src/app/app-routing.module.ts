3673e4e6c6851e72d9846db275c14cfd34d24790
Vitalik Pretsel <v.pretsel@gmail.com>
2021-08-16 15:54:45 +0300

    infinite-scroll implementation

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { UsersComponent } from './users/users.component';
+import { InfiniteScrollComponent } from './infinite-scroll/infinite-scroll.component';
 import { AuthGuard } from './_helpers/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'users', component: UsersComponent, canActivate: [AuthGuard] }
+  { path: 'users', component: UsersComponent, canActivate: [AuthGuard] },
+  { path: 'inf-scroll', component: InfiniteScrollComponent },
 ];
 
 @NgModule({
--- UI/src/app/infinite-scroll/infinite-scroll.component.ts
+++ UI/src/app/infinite-scroll/infinite-scroll.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PostsService } from '../_services/posts.service';
+
 @Component({
   selector: 'app-infinite-scroll',
   templateUrl: './infinite-scroll.component.html',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfiniteScrollComponent implements OnInit {
 
-  constructor() { }
+  allPost: Array<any>;
 
-  ngOnInit(): void {
+  constructor(private service: PostsService) {
+    this.allPost = new Array<any>();
+    this.loadPosts();
   }
 
+  ngOnInit(): void { }
+
+  loadPosts(lastId = -1) {
+    this.service.getPosts(lastId).subscribe(data => {
+      let startIndex = this.allPost.length;
+      this.allPost = this.allPost.concat(data);
+      for (let i = startIndex; i < this.allPost.length; i++) {
+        this.getLikesAmount(this.allPost[i].id, i);
+        this.getRepliesAmount(this.allPost[i].id, i);
+        this.getProfileName(this.allPost[i].profileId, i);
+      }
+    });
+  }
+
+  getLikesAmount(postId, postIndex) {
+    this.service.getLikesAmount(postId).subscribe(data => {
+      this.allPost[postIndex].likesAmount = data;
+    });
+  }
+
+  getRepliesAmount(postId, postIndex) {
+    this.service.getRepliesAmount(postId).subscribe(data => {
+      this.allPost[postIndex].repliesAmount = data;
+    });
+  }
+
+  getProfileName(profileId, postIndex) {
+    this.service.getProfileName(profileId).subscribe(data => {
+      this.allPost[postIndex].profileName = data;
+    });
+  }
+
+  onScroll() {
+    let lastId = this.allPost[this.allPost.length - 1].id - 1;
+    this.loadPosts(lastId);
+  }
 }
