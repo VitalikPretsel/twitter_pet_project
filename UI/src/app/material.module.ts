8d6429592d88b1e86870f59d335b84e531f50537
Vitalik Pretsel <v.pretsel@gmail.com>
2021-08-25 18:58:24 +0300

    added tabs to profile page

--- UI/src/app/material.module.ts
+++ UI/src/app/material.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs'
 
 const modules = [
   MatCardModule,
   MatInputModule,
-  MatButtonModule
+  MatButtonModule,
+  MatTabsModule
 ];
 
 @NgModule({
