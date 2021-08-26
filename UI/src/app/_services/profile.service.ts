652dc728b1e2d84c338041eef4998ea761e723b6
Vitalik Pretsel <v.pretsel@gmail.com>
2021-08-26 11:39:39 +0300

    created profile service

---
+++ UI/src/app/_services/profile.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private http: HttpClient) { }
+
+  public getProfile(profileName) {
+    return this.http.get(`${environment.apiUrl}/profiles/getbyname/${profileName}`);
+  }
+}
