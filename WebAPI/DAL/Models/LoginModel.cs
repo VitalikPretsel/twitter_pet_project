7309a4697318a34f3f945654f772403cd6b8bf88
Vitalik Pretsel <v.pretsel@gmail.com>
2021-09-13 20:01:21 +0300

    updated signup and login models with validation attributes

--- WebAPI/DAL/Models/LoginModel.cs
+++ WebAPI/DAL/Models/LoginModel.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DAL.Models
 {
     public class LoginModel
     {
+        [Required]
         public string UserName { get; set; }
-        
+
+        [Required]
         public string Password { get; set; }
     }
 }
--- WebAPI/DAL/Models/SignupModel.cs
+++ WebAPI/DAL/Models/SignupModel.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DAL.Models
 {
     public class SignupModel
     {
+        [Required]
         public string UserName { get; set; }
-
+        
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
-
+        
+        [Required]
         public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
     }
 }
