52c2c85b3acfb7c7f4c418f5af0cdd2e6e68ac8f
Vitalik Pretsel <v.pretsel@gmail.com>
2021-09-15 15:50:30 +0300

    fixed validation

--- WebAPI/DAL/Models/LoginModel.cs
+++ WebAPI/DAL/Models/LoginModel.cs
@@ -5,9 +5,11 @@ namespace DAL.Models
     public class LoginModel
     {
         [Required]
+        [MaxLength(256)]
         public string UserName { get; set; }
 
         [Required]
+        [MaxLength(256)]
         public string Password { get; set; }
     }
 }
--- WebAPI/DAL/Models/SignupModel.cs
+++ WebAPI/DAL/Models/SignupModel.cs
@@ -5,16 +5,20 @@ namespace DAL.Models
     public class SignupModel
     {
         [Required]
+        [MaxLength(256)]
         public string UserName { get; set; }
         
         [Required]
         [EmailAddress]
+        [MaxLength(256)]
         public string Email { get; set; }
         
         [Required]
+        [MaxLength(256)]
         public string Password { get; set; }
 
         [Required]
+        [MaxLength(256)]
         [Compare("Password")]
         public string PasswordConfirm { get; set; }
     }
