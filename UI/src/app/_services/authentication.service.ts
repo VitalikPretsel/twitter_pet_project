548bf0800baa70e2dec53b0b83ff67abc02e1a64
Vitalik Pretsel <v.pretsel@gmail.com>
2021-09-22 19:42:04 +0300

    updated auth service with refresh token

--- UI/src/app/_services/authentication.service.ts
+++ UI/src/app/_services/authentication.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 
@@ -15,7 +16,12 @@ export class AuthenticationService {
   }
 
   login(loginValues: object) {
-    return this.http.post<void>(`${environment.apiUrl}/auth/login`, loginValues);
+    return this.http.post<void>(`${environment.apiUrl}/auth/login`, loginValues)
+      .pipe(map(res => {
+        console.log("login");
+        this.startRefreshTokenTimer();
+        return res;
+      }));
   }
 
   signup(signupValues: object) {
@@ -23,6 +29,27 @@ export class AuthenticationService {
   }
 
   logout() {
+    this.stopRefreshTokenTimer();
     return this.http.delete<void>(`${environment.apiUrl}/auth`);
+  }
+
+  refreshToken() {
+    return this.http.post<void>(`${environment.apiUrl}/auth/refresh`, {})
+      .pipe(map(() => {
+        console.log("refresh token called");
+        this.startRefreshTokenTimer();
+      }));
+  }
+
+  private refreshTokenTimeout;
+
+  private startRefreshTokenTimer() {
+    console.log("refresh token timer called");
+    const timeout = 60 * 1000;
+    this.refreshTokenTimeout = setTimeout(() => this.refreshToken().subscribe(), timeout);
+  }
+
+  private stopRefreshTokenTimer() {
+    clearTimeout(this.refreshTokenTimeout);
   }
 }
