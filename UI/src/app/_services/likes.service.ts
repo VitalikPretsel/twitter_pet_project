009a4e5fea962a51815417025013d81c90121a04
Vitalik Pretsel <v.pretsel@gmail.com>
2021-10-07 16:19:22 +0300

    added likes service

---
+++ UI/src/app/_services/likes.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LikesService {
+
+  constructor(private http: HttpClient) { }
+
+  public isLiked(profileId, postId) {
+    return this.http.get<boolean>(`${environment.apiUrl}/likess/details?profileId=${profileId}&postId=${postId}`);
+  }
+
+  public like(likeValues) {
+    return this.http.post<void>(`${environment.apiUrl}/likes`, likeValues);
+  }
+
+  public unlike(profileId, postId) {
+    return this.http.delete<void>(`${environment.apiUrl}/likes/details?profileId=${profileId}&postId=${postId}`);
+  }
+}
