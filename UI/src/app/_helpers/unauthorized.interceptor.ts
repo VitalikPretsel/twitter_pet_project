d57d55517c37ff41763cec069e24011d7e9b8de0
Vitalik Pretsel <v.pretsel@gmail.com>
2021-10-11 12:00:47 +0300

    added unathorized interceptor

---
+++ UI/src/app/_helpers/unauthorized.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../_services/authentication.service';
+
+@Injectable()
+export class UnauthorizedInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router, private authenticationService: AuthenticationService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(tap(() => { },
+      async (err) => {
+        if (err.status == 401) {
+          this.authenticationService.refreshToken()
+            .subscribe(() => { }, () => {
+              this.router.navigate(['/']);
+            })
+        }
+      }));
+  }
+}
